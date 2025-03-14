ef7053ac56518a13f6986170ce34a0e76a5b6819
arkokundu200 <arkokundu200@gmail.com>
2025-03-14 17:28:34 +0530

    link - update

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -39,7 +39,7 @@ const projects = [
     description: "A platform that provides virtual labs for the students to perform experiments. It is a group project done during Felicity Hackathon of IIIT Hyderabad.",
     image: "/V Labs.png",
     tags: ["Next.js", "Tailwind CSS", "TypeScript","Responsive Design"],
-    liveLink: "#",
+    liveLink: "https://vlabs-2025-sr.vercel.app/",
     githubLink: "https://github.com/arkokundu500/VirtualLabs",
   },
   {
